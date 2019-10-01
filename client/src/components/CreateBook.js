19c47b08effa9c9af9a74d4d652d09d638b1a80e
Long Do <longdo@Longs-MacBook-Air-2.local>
2019-10-01 12:25:08 +0700

    add function to add book to database

--- client/src/components/CreateBook.js
+++ client/src/components/CreateBook.js
@@ -3,13 +3,32 @@ import {Link} from 'react-router-dom';
 import '../App.css';
 import axios from 'axios';
 
-function CreateBook() {
-    const [title, setTitle] = useState('');
-    const [isbn, setIsbn] = useState('');
-    const [author, setAuthor] = useState('');
-    const [description, setDescription] = useState('');
-    const [publishedDate, setPublishedDate] = useState('');
-    const [publisher, setPublisher] = useState('');
+const initialState = {
+    title: '',
+    isbn:'',
+    author:'',
+    description:'',
+    published_date:'',
+    publisher:''
+};
+
+function CreateBook(props) {
+    const [inputs, setInputs] = useState(initialState);
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post('http://localhost:8082/api/books', inputs)
+            .then(res => {
+                setInputs({...initialState});
+                props.history.push('/');
+            })
+            .catch(err => console.log("Error in CreateBook!"));
+    }
+
+    const handleInputChange = (e) => {
+        e.persist();
+        setInputs({...inputs, [e.target.name]: e.target.value});
+    }
 
     return (
         <div className="CreateBook">
@@ -26,25 +45,25 @@ function CreateBook() {
                         <p className="lead text-center">
                             Create New Book
                         </p>
-                        <form noValidate /* onSubmit={this.handleSubmit} */>
+                        <form noValidate onSubmit={handleSubmit}>
                             <div className="form-group">
-                                <input type="text" placeholder="Title of the Book" name="title" className="form-control" value={title} /* onChange={this.handleChange} */ />
+                                <input type="text" placeholder="Title of the Book" name="title" className="form-control" value={inputs.title} onChange={handleInputChange} />
                             </div>
                             <br />
                             <div className="form-group">
-                                <input type="text" placeholder="ISBN" name="isbn" className="form-control" value={isbn} /* onChange={this.handleChange} */ />
+                                <input type="text" placeholder="ISBN" name="isbn" className="form-control" value={inputs.isbn} onChange={handleInputChange} />
                             </div>
                             <div className="form-group">
-                                <input type="text" placeholder="Author" name="author" className="form-control" value={author} /* onChange={this.handleChange} */ />
+                                <input type="text" placeholder="Author" name="author" className="form-control" value={inputs.author} onChange={handleInputChange} />
                             </div>
                             <div className="form-group">
-                                <input type="text" placeholder="Describe this book" name="description" className="form-control" value={description} /* onChange={this.handleChange} */ />
+                                <input type="text" placeholder="Describe this book" name="description" className="form-control" value={inputs.description} onChange={handleInputChange} />
                             </div>
                             <div className="form-group">
-                                <input type="date" placeholder="Published date" name="publishedDate" className="form-control" value={publishedDate} /* onChange={this.handleChange} */ />
+                                <input type="date" placeholder="Published date" name="publishedDate" className="form-control" value={inputs.publishedDate} onChange={handleInputChange} />
                             </div>
                             <div className="form-group">
-                                <input type="text" placeholder="Publisher of this Book" name="publisher" className="form-control" value={publisher} /* onChange={this.handleChange} */ />
+                                <input type="text" placeholder="Publisher of this Book" name="publisher" className="form-control" value={inputs.publisher} onChange={handleInputChange} />
                             </div>
                             <input type="submit" className="btn btn-outline-warning btn-block mt-4" />
                         </form>
