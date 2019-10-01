6ecff752c1237145e41c4ebaf035a83000604457
Long Do <longdo@Longs-MacBook-Air-2.local>
2019-10-01 13:26:45 +0700

    display book

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,17 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import './App.css';
 
 import CreateBook from './components/CreateBook';
 import ShowBookList from './components/ShowBookList';
-import ShowBookDetails from './components/ShowBookDetails';
-import UpdateBookInfo from './components/UpdateBookInfo';
+/* import ShowBookDetails from './components/ShowBookDetails';
+import UpdateBookInfo from './components/UpdateBookInfo'; */
 
 function App() {
   return (
     <Router>
       <div>
-        {/* <Route exact path='/' component={ShowBookList} /> */}
+        <Route exact path='/' component={ShowBookList} />
         <Route path='/create-book' component={CreateBook} />
         {/* <Route path='/edit-book/:id' component={UpdateBookInfo} /> */}
         {/* <Route path='/show-book/:id' component={ShowBookDetails} /> */}
