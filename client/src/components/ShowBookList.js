c66fd3f2f7c77d9ee33f0decbc13255f6933c415
Long Do <longdx@vfdcorp.com>
2019-10-01 14:56:23 +0700

    refactor code

--- client/src/components/ShowBookList.js
+++ client/src/components/ShowBookList.js
@@ -16,34 +16,27 @@ function ShowBookList(){
                 console.log('Error from ShowBookList');
             });
     }, []);
-    
-    let bookList;
-    if (books.length === 0) {
-        bookList = "there is no book record!";
-    } else {
-        bookList = books.map((book, k) => <BookCard book={book} key={k} />);
-    }
 
     return(
         <div className="ShowBookList">
             <div className="container">
-            <div className="row">
-                <div className="col-md-12">
-                <br />
-                <h2 className="display-4 text-center">Books List</h2>
+                <div className="row">
+                    <div className="col-md-12">
+                        <br />
+                        <h2 className="display-4 text-center">Books List</h2>
+                    </div>
+                    <div className="col-md-11">
+                        <Link to="/create-book" className="btn btn-outline-warning float-right">
+                            + Add New Book
+                        </Link>
+                        <br />
+                        <br />
+                        <hr />
+                    </div>
                 </div>
-                <div className="col-md-11">
-                <Link to="/create-book" className="btn btn-outline-warning float-right">
-                    + Add New Book
-                </Link>
-                <br />
-                <br />
-                <hr />
+                <div className="list">
+                    {books.map((book, k) => <BookCard book={book} key={k} />)}
                 </div>
-            </div>
-            <div className="list">
-                {bookList}
-            </div>
             </div>
         </div> 
     );
