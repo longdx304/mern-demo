ffd9940b27e516e1daafb541b2865cf25de4be08
Long Do <longdo@Longs-MacBook-Air-2.local>
2019-10-01 12:24:35 +0700

    add CORS dependency

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 const PORT = process.env.PORT || 8082;
 
 app.use(bodyParser.json());
+app.use(cors({ origin: true, credentials: true }));
 
 //connect db
 connectDB();
