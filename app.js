9be6149c75a32960fa8aba3022864f80749ab292
Long Do <longdx@vfdcorp.com>
2019-10-02 15:45:56 +0700

    edit code for production

--- app.js
+++ app.js
@@ -2,23 +2,26 @@ const express = require('express');
 const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 8082;
 
 app.use(bodyParser.json());
 app.use(cors({ origin: true, credentials: true }));
+//Adds the react production build to serve react requests
+app.use(express.static(path.join(__dirname, './client/build')));
 
 //connect db
 connectDB();
 
-app.get('/', (req, res) => {
-    res.send('Hello world!');
-});
-
 //mount books api
 const booksRouter = require('./routes/api/books');
 app.use('/api/books', booksRouter);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './client/build/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
